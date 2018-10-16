ac4e7c20acbca2db8693e5c230fe40fb5333e056
Rajat Jain <rajatjain@WIN-ET3344NOCA4.redmond.corp.microsoft.com>
2018-10-16 22:41:24 +0530

    intial code setup and fetched data from api

---
+++ src/components/list/List.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { handleResponse } from '../../helpers';
+import { API_URL } from '../../config';
+
+class List extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            loading: false,
+            currencies: [],
+            error: null
+        };
+    }
+    componentDidMount() {
+        this.setState({ loading: true })
+        fetch(`${API_URL}/cryptocurrencies?page=1&perPage=20`)
+        .then(handleResponse)
+        .then((data) => {
+            this.setState({
+                currencies: data.currencies,
+                loading: false 
+            })
+        })
+        .catch((error) => {
+            this.setState({
+                error: error.errorMessage, 
+                loading: false 
+            })
+        });
+    }
+    render() {
+        console.log(this.state)
+        if(this.state.loading) {
+            return (
+                <div>Loading...</div>
+            )
+        }
+        return(
+            <div>text</div>
+        )
+    }
+}
+
+export default List;
